16ecc702c5e215c5581922f589a44a90a2314afd
Michael Burak <michaelrburak@gmail.com>
2018-05-10 12:10:47 -0400

    commented solution to examining tree level width

--- exercises/levelwidth/index.js
+++ exercises/levelwidth/index.js
@@ -11,6 +11,33 @@
 // 4       5
 // Answer: [1, 3, 2]
 
-function levelWidth(root) {}
+function levelWidth(root) {
+    //whenever you see width, think of using breadth-first first!
+    //generally you will have to write a function to  traverse AND 
+    //do work while traversing in interviews, not just a BF or DF traverse
+
+    //array with root and stopper init
+    const arr = [root, 's']
+    //to count width, could also be called 'width'
+    const counters = 0
+
+    while (arr.length > 1) {
+
+        //pull out next element
+        const node = arr.shift()
+        if (node === 's') {
+            //signifies moving on to another row
+            counters.push(0)
+            //returns stopper to end of array
+            arr.push('s')
+        } else {
+            //add node's children to array if node
+            arr.push(...node.children)
+            //find last element, increment by one as last element is current level width
+            counters[counters.length - 1]++
+        }
+    }
+    return counters
+}
 
 module.exports = levelWidth;
