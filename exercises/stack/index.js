fef83d808ce5abf05db2b7714d2f25b80152d4fc
Michael Burak <michaelrburak@gmail.com>
2018-05-10 12:12:28 -0400

    commented pushing popping and peeking of stacks solution

--- exercises/stack/index.js
+++ exercises/stack/index.js
@@ -10,6 +10,23 @@
 //   s.pop(); // returns 2
 //   s.pop(); // returns 1
 
-class Stack {}
+class Stack {
+
+    constructor() {
+        //have to call this in constructors!
+        this.data = [];
+    }
+    push(record) {
+        this.data.push(record);
+    }
+    pop() {
+        //return that removed boi
+        return this.data.pop()
+    }
+    peek() {
+        //return last index'd data
+        return this.data[this.data.length - 1]
+    }
+}
 
 module.exports = Stack;
