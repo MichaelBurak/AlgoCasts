13c8959f02f9f9fb2ddd2652dd2cb6db52705496
Michael Burak <michaelrburak@gmail.com>
2018-05-10 12:12:48 -0400

    commented node structure and traversal of tree

--- exercises/tree/index.js
+++ exercises/tree/index.js
@@ -10,8 +10,53 @@
 // on the tree class.  Each method should accept a
 // function that gets called with each element in the tree
 
-class Node {}
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.children = [];
+    }
+    add(data) {
+        this.children.push(new Node(data))
+    }
 
-class Tree {}
+    remove(data) {
+        this.children = this.children.filter(node => {
+            //return true for every element that is not 'data', removes every
+            //non match, does not mutate original array except
+            //that it is explicitly setting this.children!
+            return node.data !== data
+        })
+    }
+}
+
+class Tree {
+    constructor() {
+        this.root = null
+
+    }
+    traverseBF(fn) {
+        //puts root node in array to iterate over
+        const arr = [this.root]
+        //while the array has something in it 
+        while (arr.length) {
+            //shift = base array method that takes out 1st element
+            const node = arr.shift();
+            //sadly node.children is an array, so would produce a nested array if 
+            //pushed
+            arr.push(...node.children);
+            fn(node);
+        }
+    }
+    traverseDF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift()
+            //adds to front of the array
+            arr.unshift(...node.children)
+            fn(node)
+        }
+    }
+}
+
 
 module.exports = { Tree, Node };
